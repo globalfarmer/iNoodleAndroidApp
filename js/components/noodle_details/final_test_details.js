7b3140f566c114fb7a16301203517a402e7a4460
DoVietAnh <anhdv@projectkit.net>
2017-05-16 09:03:37 +0700

    issue #21 impl finaltest details ui

--- js/components/noodle_details/final_test_details.js
+++ js/components/noodle_details/final_test_details.js
@@ -1,28 +1,55 @@
 
 import React, { Component } from 'react';
-import { TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
-import { Actions, ActionConst } from 'react-native-router-flux';
-import { Container, Content, Text, Button} from 'native-base';
+import { Container, Content, Text, Card, CardItem, Body } from 'native-base';
 import { Grid, Row } from 'react-native-easy-grid';
 
-import { openDrawer } from '../../actions/drawer';
 import styles from './styles';
 import { FINAL_TEST } from '../../actions/noodleboard';
 
 
 class FinalTestDetails extends Component {
-
-    static propTypes = {
-        name: React.PropTypes.string,
-        openDrawer: React.PropTypes.func,
+    getStartTime = (uploadTime) => {
+        return `${uploadTime.toLocaleTimeString()}, ${uploadTime.getDate()}-${uploadTime.getMonth()+1}-${uploadTime.getFullYear()}`;
     }
 
     render() {
         return (
             <Container style={styles.container}>
-                <Content>
-                    <Text>{this.props.name + "_DETAILS"}</Text>
+                <Content style={{margin: 10}}>
+                  <Card>
+                      <CardItem cardBody>
+                          <Body style={{flex: 1}}>
+                              <Text style={{marginLeft: 10, marginTop: 5, marginRight: 5, marginBottom: 5,fontSize: 18}}>
+                                  {this.props.ft.course.code}
+                              </Text>
+                              <Text style={{marginLeft: 10, marginTop: 5, marginRight: 5, marginBottom: 5,fontSize: 18}}>
+                                  {this.props.ft.course.name}
+                              </Text>
+                              <Text style={{marginLeft: 10, marginTop: 5, marginRight: 5, marginBottom: 5,fontSize: 18}}>
+                                  {this.getStartTime(new Date(this.props.ft.time))}
+                              </Text>
+                              <Text style={{marginLeft: 10, marginTop: 5, marginRight: 5, marginBottom: 5,fontSize: 18}}>
+                                  So bao danh {this.props.ft.seat}
+                              </Text>
+                              <Text style={{marginLeft: 10, marginTop: 5, marginRight: 5, marginBottom: 5,fontSize: 18}}>
+                                  Ca thi {this.props.ft.sessionNo}
+                              </Text>
+                              <Text style={{marginLeft: 10, marginTop: 5, marginRight: 5, marginBottom: 5,fontSize: 18}}>
+                                  {this.props.ft.room}
+                              </Text>
+                              <Text style={{marginLeft: 10, marginTop: 5, marginRight: 5, marginBottom: 5,fontSize: 18}}>
+                                  {this.props.ft.area}
+                              </Text>
+                              <Text style={{marginLeft: 10, marginTop: 5, marginRight: 5, marginBottom: 5,fontSize: 18}}>
+                                  {this.props.ft.term}
+                              </Text>
+                              <Text style={{marginLeft: 10, marginTop: 5, marginRight: 5, marginBottom: 5,fontSize: 18}}>
+                                  {this.props.ft.type}
+                              </Text>
+                          </Body>
+                      </CardItem>
+                  </Card>
                 </Content>
             </Container>
         );
@@ -30,8 +57,7 @@ class FinalTestDetails extends Component {
 }
 
 const mapStateToProps = state => ({
-  name: state.noodleDetails.boardSource,
-  data: state.noodleDetails.data
+  ft: state.noodleDetails.data
 });
 
 export default connect(mapStateToProps)(FinalTestDetails);
